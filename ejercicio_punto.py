5a3f45f6bdbb0a6a59d0c88efd00461c8d7b1693
Franco Palacios <francopalacios221@gmail.com>
2026-04-06 18:57:06 -0300

    Subiendo clase Punto

--- ejercicio_punto.py
+++ ejercicio_punto.py
@@ -23,3 +23,36 @@ print ("Coordenada opuesta en el eje x:", punto1.opuesto().eje_x(),
     
 
 #ejercico 3
+class Linea:
+    def __init__ (self, punto_a, punto_b):
+        self.punto_a = punto_a
+        self.punto_b = punto_b
+
+    def mueve_derecha (self, distancia):
+        self.punto_a.x += distancia
+        self.punto_b.x += distancia
+
+    def mueve_izquierda (self, distancia):
+        self.punto_a.x -= distancia
+        self.punto_b.x -= distancia
+
+    def mueve_arriba (self, distancia):
+        self.punto_a.y += distancia
+        self.punto_b.y += distancia
+
+    def mueve_abajo (self, distancia):
+        self.punto_a.y -= distancia
+        self.punto_b.y -= distancia
+
+punto_a = Punto(1, 2)
+punto_b = Punto(3, 4)
+linea = Linea(punto_a, punto_b)
+print("Coordenadas iniciales:")
+print("Punto A: (", linea.punto_a.eje_x(), ",", linea.punto_a.eje_y(), ")")
+print("Punto B: (", linea.punto_b.eje_x(), ",", linea.punto_b.eje_y(), ")")
+linea.mueve_derecha(2)
+linea.mueve_arriba(1)
+print("Coordenadas después del movimiento:")
+print("Punto A: (", linea.punto_a.eje_x(), ",", linea.punto_a.eje_y(), ")")
+print("Punto B: (", linea.punto_b.eje_x(), ",", linea.punto_b.eje_y(), ")")
+
